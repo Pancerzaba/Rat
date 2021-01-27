2bf2d98dd596f9ce2cb53380611a581944e49943
Pancerzaba <marta.rozmyslowicz21@gmail.com>
2021-01-27 23:39:42 +0100

    add Character Combat

---
+++ Assets/scripts/CharacterCombat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCombat : MonoBehaviour
+{
+    public CharacterStatus myStats;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myStats = GetComponent<CharacterStatus>();
+    }
+
+    public void MealeAttack (CharacterStatus targetStats)
+    {
+        targetStats.TakeDamage(myStats.damage);
+    }
+
+}
